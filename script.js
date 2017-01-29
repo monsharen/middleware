5204599ba907c9fa17aa2ce22703438f8bff96e6
monsharen <thomas.rosenquist@flexionmobile.com>
2017-01-29 16:45:46 +0000

    first commit

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ middlewareApp.controller('MiddlewareController', ['$scope', '$http', '$location'
 		}		
 	};
 
+	this.send = function() {
+		window.location.href = "https://monsharen.github.io/middleware/mockserver.html#/access_token=" + ctrl.authenticationToken;
+	};
+
 	authService.isUserAuthenticated(
 		function (token) {
 			console.log("user is authenticated");
@@ -27,5 +31,49 @@ middlewareApp.controller('MiddlewareController', ['$scope', '$http', '$location'
 			console.log("user is not authenticated");
 		}
 	);
+
+	$.ajax({
+	    url: 'https://url.com',
+	    type: 'post',
+	    data: {
+	        access_token: 'XXXXXXXXXXXXXXXXXXX'
+	    },
+	    headers: {
+	        Header_Name_One: 'Header Value One',   //If your header name has spaces or any other char not appropriate
+	        "Header Name Two": 'Header Value Two'  //for object property name, use quoted notation shown in second
+	    },
+	    dataType: 'json',
+	    success: function (data) {
+	        console.info(data);
+	    }
+	});
+	
+}]);
+
+
+
+var middlewareServerApp = angular.module('middlewareServerApp');
+
+middlewareServerApp.controller('MiddlewareServerController', ['$scope', '$http', '$location', 
+	function($scope, $http, $location) {
+
+	var ctrl = this;
+	this.extensionIds = ["1234"];
+	this.authenticationToken = getHashParams()["/access_token"];
+
+	this.getHashParams = function() {
+
+	    var hashParams = {};
+	    var e,
+	        a = /\+/g,  // Regex for replacing addition symbol with a space
+	        r = /([^&;=]+)=?([^&;]*)/g,
+	        d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+	        q = window.location.hash.substring(1);
+
+	    while (e = r.exec(q))
+	       hashParams[d(e[1])] = d(e[2]);
+
+	    return hashParams;
+	};
 	
 }]);
